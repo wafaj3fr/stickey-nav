79409983a222cdb25b8a02142e8ab1c8f277c258
wafa j3fr <wafaagaafar@gmail.com>
2023-11-19 11:03:34 +0200

    all files

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var header = document.getElementById('header');
+    var page = document.getElementById('page');
+    var openMenuButton = document.getElementById('openmenu');
+
+    window.addEventListener('scroll', function(){
+        page.classList.remove('menuopen');
+        if (window.scrollY >= 100){
+            header.classList.add('sticky');
+        }
+        else{
+            header.classList.remove('sticky');
+        }
+    });
+
+    openMenuButton.addEventListener('click', function(){
+        header.classList.remove('sticky');
+        page.classList.add('menuopen');
+    });
+
+    var links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(function(link){
+        link.addEventListener('click', function(event){
+            event.preventDefault();
+
+            var targetId = this.getAttribute('href');
+            var targetElement = document.querySelector(targetId);
+
+            if (targetElement){
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
